a101209f420585337619bbe4e83dcaa3a1f62645
Joao luis cardoso <joao.lc12@htomail.com>
2023-10-09 12:12:18 -0300

    Trabalho est2 Java trees- interface

---
+++ BinarySearchTree/src/binarysearchtree/model/Dados.java
@@ -0,0 +1,59 @@
+package binarysearchtree.model;
+
+
+public class Dados implements Comparable<Dados>{
+   private int ibgeCode;
+   private String cidade;
+   private int pop2018;
+   private double IDH;
+   private double pib2014;
+
+    public int getIbgeCode() {
+        return ibgeCode;
+    }
+
+    public void setIbgeCode(int ibgeCode) {
+        this.ibgeCode = ibgeCode;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getPop2018() {
+        return pop2018;
+    }
+
+    public void setPop2018(int pop2018) {
+        this.pop2018 = pop2018;
+    }
+
+    public double getIDH() {
+        return IDH;
+    }
+
+    public void setIDH(double IDH) {
+        this.IDH = IDH;
+    }
+
+    public double getPib2014() {
+        return pib2014;
+    }
+
+    public void setPib2014(double pib2014) {
+        this.pib2014 = pib2014;
+    }
+
+
+    @Override    
+    public int compareTo(Dados outroDado){
+      return this.getCidade().
+              compareTo(outroDado.getCidade());
+    }
+    
+    
+}
---
+++ BinarySearchTree/src/binarysearchtree/view/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarysearchtree.view;
+
+import binarysearchtree.Jframes.FormSistema;
+
+/**
+ *
+ * @author marci
+ */
+public class Main {
+    public static void main(String[] args) {
+        FormSistema form = new FormSistema();
+        form.setVisible(true);
+    }
+}
